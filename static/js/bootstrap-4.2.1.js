ad425d65ad23f90a486396b2d794f67fb54c2af5
kaiyer <dengfangchen233@gmail.com>
2023-11-03 16:18:05 +0800

    init

---
+++ static/js/bootstrap-4.2.1.js
@@ -0,0 +1,3 @@
+/*-防止各大cdn公共库加载地址失效问题，此地址我们会时时监控，及调整以保障正常访问-www.jq22.com为您服务*/
+/*当前为微软cdn公共库提供的bootstrap-4.2.1*/
+document.write("<script src='https://lib.sinaapp.com/js/bootstrap/4.2.1/js/bootstrap.min.js'><\/script>");
